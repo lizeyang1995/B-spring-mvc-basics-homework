410c51151b5e5c3602f547841f15694e457e1a17
lizeyang1995 <424011527@qq.com>
2020-11-03 23:43:20 +0800

    [lizeyang] feat: add a test, should throw error when username exist

--- src/test/java/com/thoughtworks/capacity/gtb/mvc/controllerTest/UserControllerTest.java
+++ src/test/java/com/thoughtworks/capacity/gtb/mvc/controllerTest/UserControllerTest.java
@@ -2,27 +2,46 @@ package com.thoughtworks.capacity.gtb.mvc.controllerTest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.capacity.gtb.mvc.domain.User;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserControllerTest {
     @Autowired
     MockMvc mockMvc;
+
     @Test
+    @Order(1)
     void should_add_a_user() throws Exception {
         User user = User.builder().username("lizeyang").password("123456").email("123@123.com").build();
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonString = objectMapper.writeValueAsString(user);
         mockMvc.perform(post("/register").content(jsonString).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
+    }
+    @Test
+    @Order(2)
+    void should_throw_error_when_username_exist() throws Exception {
+        User user = User.builder().username("lizeyang").password("123456").email("123@123.com").build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/register").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", is("用户已存在")))
+                .andExpect(jsonPath("$.code", is("400")));
     }
 }
