69d23a1f776b0b725085171cbca45e0fb5b36bc4
lizeyang1995 <424011527@qq.com>
2020-11-04 00:39:11 +0800

    [lizeyang] feat: add login api

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
@@ -3,11 +3,10 @@ package com.thoughtworks.capacity.gtb.mvc.controller;
 import com.thoughtworks.capacity.gtb.mvc.domain.User;
 import com.thoughtworks.capacity.gtb.mvc.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Size;
 
 @RestController
 public class UserController {
@@ -21,5 +20,11 @@ public class UserController {
     public ResponseEntity registerUser(@RequestBody @Valid User user) {
         userService.registerUser(user);
         return ResponseEntity.created(null).build();
+    }
+
+    @GetMapping("login")
+    public ResponseEntity login(@RequestParam @Size(max = 10, min = 3, message = "用户名不合法") String username,
+                                @RequestParam @Size(max = 12, min = 5, message = "密码不合法") String password) {
+        return ResponseEntity.ok(null);
     }
 }
