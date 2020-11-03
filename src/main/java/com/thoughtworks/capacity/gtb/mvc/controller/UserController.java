e0d172ce398591f82fb6f2a46e3e634b26060a92
lizeyang1995 <424011527@qq.com>
2020-11-04 00:55:14 +0800

    [lizeyang] feat: complete a test, should throw given informal name when login

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
@@ -3,13 +3,16 @@ package com.thoughtworks.capacity.gtb.mvc.controller;
 import com.thoughtworks.capacity.gtb.mvc.domain.User;
 import com.thoughtworks.capacity.gtb.mvc.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.Map;
 
 @RestController
+@Validated
 public class UserController {
     final UserService userService;
 
@@ -24,8 +27,11 @@ public class UserController {
     }
 
     @GetMapping("login")
-    public ResponseEntity login(@RequestParam @Size(max = 10, min = 3, message = "用户名不合法") String username,
-                                @RequestParam @Size(max = 12, min = 5, message = "密码不合法") String password) {
+    public ResponseEntity login(@RequestParam
+                                @Size(max = 10, min = 3, message = "用户名不合法")
+                                @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "用户名不合法")String username,
+                                @RequestParam
+                                @Size(max = 12, min = 5, message = "密码不合法") String password) {
         Map<String, Object> user = userService.login(username, password);
         return ResponseEntity.ok(user);
     }
--- src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/GlobalExceptionHandler.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.util.Objects;
+import java.util.Set;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -24,6 +27,19 @@ public class GlobalExceptionHandler {
     public ResponseEntity userExceptionHandler(MethodArgumentNotValidException exception) {
         String errorMessage;
         errorMessage = Objects.requireNonNull(exception.getBindingResult().getFieldError()).getDefaultMessage();
+        Error error = new Error();
+        error.setCode(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(errorMessage);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity userExceptionHandler(ConstraintViolationException exception) {
+        String errorMessage = null;
+        for (ConstraintViolation<?> constraintViolation : exception.getConstraintViolations()) {
+            errorMessage = constraintViolation.getMessage();
+            break;
+        }
         Error error = new Error();
         error.setCode(HttpStatus.BAD_REQUEST.value());
         error.setMessage(errorMessage);
