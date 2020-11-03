288ddd63156c1c2ec2eec9692a3eb59d8f84c37b
lizeyang1995 <424011527@qq.com>
2020-11-04 00:28:21 +0800

    [lizeyang] feat: complete a test, should throw error when username size invalid

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/GlobalExceptionHandler.java
@@ -2,8 +2,11 @@ package com.thoughtworks.capacity.gtb.mvc.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Objects;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,6 +14,16 @@ public class GlobalExceptionHandler {
     public ResponseEntity userExceptionHandler(UserAlreadyExistsException exception) {
         String errorMessage;
         errorMessage = exception.getMessage();
+        Error error = new Error();
+        error.setCode(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(errorMessage);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity userExceptionHandler(MethodArgumentNotValidException exception) {
+        String errorMessage;
+        errorMessage = Objects.requireNonNull(exception.getBindingResult().getFieldError()).getDefaultMessage();
         Error error = new Error();
         error.setCode(HttpStatus.BAD_REQUEST.value());
         error.setMessage(errorMessage);
