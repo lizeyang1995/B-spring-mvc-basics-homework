3ef8b289f85c57ac8d6cb54c4b7a1cad9ccdd74d
lizeyang1995 <424011527@qq.com>
2020-11-03 23:48:15 +0800

    [lizeyang] feat: add class named UserAlreadyExistsException

---
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/UserAlreadyExistsException.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.capacity.gtb.mvc.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    private String errorMessage;
+
+    public UserAlreadyExistsException(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
